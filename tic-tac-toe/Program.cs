200fe4a38c49060be088e63fd58dee2d032b800e
IlyaErmokhin <63489397+IlyaErmokhin@users.noreply.github.com>
2023-02-21 19:58:19 +0400

    Update Program.cs

--- tic-tac-toe/Program.cs
+++ tic-tac-toe/Program.cs
@@ -1,131 +1,167 @@
 ﻿using System;
 
 
-namespace TicTacToe
+namespace TicTacToe;
+
+class Program
 {
-    class Program
+    const char X = 'X';
+    const char O = 'O';
+    const char zero = ' ';
+    static int boardSize = 3;
+    private static int boardArea = boardSize * boardSize;
+
+    //Вывод поля
+    static void PrintBoard(char[] board)
     {
-        //Вывод поля
-        public static void GamingBoard(char[] arr)
+        const string gorizontal1 = "-----------------";
+        const string gorizontal2 = "_________________";
+        const string vertical1 = "|";
+        const string vertical2 = "|     |     |     |";
+
+        Console.WriteLine("\n                   Tic-Tac-Toe \n\n\n");
+        Console.WriteLine("            1 player: X,  2 player: O\n");
+        Console.WriteLine("    Gaming Board                   Square numeration\n");
+        Console.WriteLine($"  {gorizontal2}                {gorizontal2}");
+        Console.WriteLine($" {vertical2}              {vertical2}");
+        Console.WriteLine(
+            $" {vertical1}  {board[6]}  {vertical1}  {board[7]}  {vertical1}  {board[8]}  {vertical1}              |  7  |  8  |  9  |");
+        Console.WriteLine($" {vertical2}              {vertical2}");
+        Console.WriteLine($" {vertical1}{gorizontal1}{vertical1}              {vertical1}{gorizontal1}{vertical1}");
+        Console.WriteLine($" {vertical2}              {vertical2}");
+        Console.WriteLine(
+            $" {vertical1}  {board[3]}  {vertical1}  {board[4]}  {vertical1}  {board[5]}  {vertical1}              |  4  |  5  |  6  |");
+        Console.WriteLine($" {vertical2}              {vertical2}");
+        Console.WriteLine($" {vertical1}{gorizontal1}{vertical1}              {vertical1}{gorizontal1}{vertical1}");
+        Console.WriteLine($" {vertical2}              {vertical2}");
+        Console.WriteLine(
+            $" {vertical1}  {board[0]}  {vertical1}  {board[1]}  {vertical1}  {board[2]}  {vertical1}              |  1  |  2  |  3  |");
+        Console.WriteLine($" {vertical2}              {vertical2}");
+        Console.WriteLine($"  {gorizontal1}                {gorizontal1}\n");
+    }
+
+        static void Main()
+    {
+        char[] board = new char[boardArea];
+        char winner = ' ';
+        int step = 0;
+
+        Array.Fill(board, zero);
+        PrintBoard(board);
+        while (step < boardArea)
         {
-            const string gorizontal1 = "-----------------";
-            const string gorizontal2 = "_________________";
-            const string vertical1 = "|";
-            const string vertical2 = "|     |     |     |";
-
-            Console.WriteLine("");
-            Console.WriteLine("                   Tic-Tac-Toe ");
-            Console.WriteLine("");
-            Console.WriteLine(""); 
-            Console.WriteLine("");
-            Console.WriteLine("            1 player: X,  2 player: O");
-            Console.WriteLine("");
-            Console.WriteLine("    Gaming Board                   Square numeration");
-            Console.WriteLine("");
-            Console.WriteLine($"  {gorizontal2}                {gorizontal2}" );
-
-            Console.WriteLine($" {vertical2}              {vertical2}" );
-
-            Console.WriteLine($" {vertical1}  {arr[7]}  {vertical1}  {arr[8]}  {vertical1}  {arr[9]}  {vertical1}              |  7  |  8  |  9  |");
-
-            Console.WriteLine($" {vertical2}              {vertical2}" );
-
-            Console.WriteLine($" {vertical1}{gorizontal1}{vertical1}              {vertical1}{gorizontal1}{vertical1}");
-
-            Console.WriteLine($" {vertical2}              {vertical2}");
-
-            Console.WriteLine($" {vertical1}  {arr[4]}  {vertical1}  {arr[5]}  {vertical1}  {arr[6]}  {vertical1}              |  4  |  5  |  6  |");
-
-            Console.WriteLine($" {vertical2}              {vertical2}");
-
-            Console.WriteLine($" {vertical1}{gorizontal1}{vertical1}              {vertical1}{gorizontal1}{vertical1}");
-
-            Console.WriteLine($" {vertical2}              {vertical2}");
-
-            Console.WriteLine($" {vertical1}  {arr[1]}  {vertical1}  {arr[2]}  {vertical1}  {arr[3]}  {vertical1}              |  1  |  2  |  3  |");
-
-            Console.WriteLine($" {vertical2}              {vertical2}");
-
-            Console.WriteLine($"  {gorizontal1}                {gorizontal1}");
-            Console.WriteLine("");
+            var player = (step % 2) + 1;
+            ChampionPlayer(board, player);
+            winner = CheckWin(board);
+            if (winner != zero)
+                break;
+            step++;
         }
 
-        static void Main(string[] args)
-        {
-            const char zero = ' ';
-            char[] Field = new char[10];
-            int step = 0;
-            bool game = false;
+        if (step == boardArea - 1)
+            Console.WriteLine(" It's a draw!");
+        else
+            Console.WriteLine($" player {(winner == X ? 1 : 2)} won!");
 
-            Array.Fill(Field, zero);
-            GamingBoard(Field);
-            while (game == false)
-            {
-                game = ChampionPlayer(zero, Field, game, step);
-                step += 1;
-            }
-            Console.ReadKey();
-        }
+        Console.WriteLine("Press enter to exit");
+        Console.ReadLine();
+    }
 
         //Ввод данных и определение победителя
-        public static bool ChampionPlayer(char Zero, char[] arr, bool GameCheck, int step)
+    static void ChampionPlayer(char[] arr, int player)
+    {
+        int selectedNumber;
+
+        while (true)
         {
-            const char X = 'X';
-            const char O = 'O';
-            int player, number;
-            string s = " ";
-            bool flag = true;
-            player = (step % 2) + 1;
-
-            do
+            Console.WriteLine($"\n                   {player} player move:\n");
+            Console.Write($" Select a cell from 1 to {boardArea}: ");
+            string s = Console.ReadLine();
+            try
             {
-                try
-                {
-                    Console.WriteLine("");
-                    Console.WriteLine("                   " + player + " player move:");
-                    Console.WriteLine("");
-                    Console.Write(" Select a cell from 1 to 9: ");
-                    s = Console.ReadLine();
-                    number = Convert.ToInt32(s);
-                    if ((s.Length == 1) && (number >= 1) && (number <= 9))
-                    {
-                        if (arr[number] == Zero)
-                        {
-                            if (player == 1)
-                                arr[number] = X;
-                            else arr[number] = O;
-                            flag = false;
-                        }
-                        else Console.WriteLine(" Goddamn, this cell is occupied!");
-                    }
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine(" Goddamn, please enter the cell number from 1 to 9!");
-                }
+                selectedNumber = Convert.ToInt32(s);
+                selectedNumber--;
+                if (selectedNumber < 0 || selectedNumber > boardArea - 1)
+                    throw new FormatException();
             }
-            while (flag);
-
-            //Victory check
-            GameCheck = (Zero != arr[1]) && (arr[1] == arr[2]) && (arr[2] == arr[3]) ||
-            (Zero != arr[4]) && (arr[4] == arr[5]) && (arr[5] == arr[6]) ||
-            (Zero != arr[7]) && (arr[7] == arr[8]) && (arr[8] == arr[9]) ||
-            (Zero != arr[1]) && (arr[1] == arr[4]) && (arr[4] == arr[7]) ||
-            (Zero != arr[2]) && (arr[2] == arr[5]) && (arr[5] == arr[8]) ||
-            (Zero != arr[3]) && (arr[3] == arr[6]) && (arr[6] == arr[9]) ||
-            (Zero != arr[1]) && (arr[1] == arr[5]) && (arr[5] == arr[9]) ||
-            (Zero != arr[3]) && (arr[3] == arr[5]) && (arr[5] == arr[7]);
-            Console.Clear();
-            GamingBoard(arr);
-
-            if (GameCheck)
-                Console.WriteLine(" " + player + " player " + " won!");
-            else if (step == 8)
+            catch (FormatException)
             {
-                GameCheck = true;
-                Console.WriteLine(" It's a draw!");
+                Console.WriteLine(" Goddamn, please enter the cell number from 1 to 9!");
+                continue;
             }
-            return GameCheck;
+            catch (Exception)
+            {
+                continue;
+            }
+
+            if (arr[selectedNumber] == zero)
+            {
+                arr[selectedNumber] = player == 1 ? X : O;
+                break;
+            }
+
+            Console.WriteLine(" Goddamn, this cell is occupied!");
         }
+
+        Console.Clear();
+        PrintBoard(arr);
+    }
+
+            private static char CheckWin(char[] board)
+    {
+        var columnsCheck = new int[boardSize];
+        Array.Fill(columnsCheck, 0);
+        var rowsCheck = new int[boardSize];
+        Array.Fill(rowsCheck, 0);
+        var diagonal = new char[boardSize];
+        var oppositeDiagonal = new char[boardSize];
+
+        for (var i = 0; i < boardSize; i++)
+        {
+            for (var j = 0; j < boardSize; j++)
+            {
+                var currentField = board[i * boardSize + j];
+                switch (currentField)
+                {
+                    case X:
+                        columnsCheck[j]++;
+                        rowsCheck[i]++;
+                        break;
+                    case O:
+                        columnsCheck[j]--;
+                        rowsCheck[i]--;
+                        break;
+                }
+
+                if (i == j)
+                {
+                    diagonal[i] = currentField;
+                    oppositeDiagonal[i] = board[i * boardSize + boardSize - j - 1];
+                }
+            }
+        }
+
+
+        if (
+            columnsCheck.Any(x => x == boardSize)
+            || rowsCheck.Any(x => x == boardSize)
+            || diagonal.All(x => x == X)
+            || oppositeDiagonal.All(x => x == X)
+        )
+        {
+            return X;
+        }
+
+        if (
+            columnsCheck.Any(x => x == -boardSize)
+            || rowsCheck.Any(x => x == -boardSize)
+            || diagonal.All(x => x == O)
+            || oppositeDiagonal.All(x => x == O)
+        )
+        {
+            return O;
+        }
+
+        return zero;
     }
 }
